d16cde60bce885e2feee83c01a33c0760ba19deb
Kasutaja Kasutaja <ajurgens@tktk.ee>
2022-03-19 17:52:30 +0200

    Loop and loop

---
+++ DiceGame/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb kaks mängijat - arvuti (cpu) ja kasutaja (user);
+            //mõlemad mängijad viskavad täringuid;
+            //programm kontrollib kumb mängija viskas rohkem
+            //mängija, kes viskab rohkem, ongi mängu võitja
+            //*täringuid visatakse kolm korda
+            //programm kuulutab võitja
+
+            Random rnd = new Random();
+
+
+            int cpuScore = 0;
+            int userScore = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                int cpuRandom = rnd.Next(1, 7);
+                int userRandom = rnd.Next(1, 7);
+
+                Console.WriteLine($"Arvuti viskas {cpuRandom}. Kasutaja viskas {userRandom}");
+
+                if (cpuRandom < userRandom)
+                {
+                    Console.WriteLine("Kasutaja on viske võitnud. Palju õnne!");
+                    userScore = userScore + 1;
+                }
+                else if (cpuRandom > userRandom)
+                {
+                    Console.WriteLine("Arvuti on viske võitnud.");
+                    cpuScore = cpuScore + 1;
+                }
+                else
+                {
+                    Console.WriteLine("Viik!");
+                }
+            }
+
+            if(cpuScore < userScore)
+            {
+                Console.WriteLine("Kasutaja on mängu võitnud. Palju õnne!");
+                
+            }
+            else if (cpuScore > userScore)
+            {
+                Console.WriteLine("Arvuti on mängu võitnud.");
+                
+            }
+            else
+            {
+                Console.WriteLine("Viik!");
+            }
+
+
+
+        }
+    }
+}
